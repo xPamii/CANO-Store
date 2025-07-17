ef68323852f56efcf610e66a438beb6f9b4658dc
xPamii <pbherath2018@gmail.com>
2025-07-18 00:16:34 +0530

    User Entity class

---
+++ src/java/controller/User.java
@@ -0,0 +1,112 @@
+package controller;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pamii
+ */
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", length = 45, nullable = false)
+    private String first_name;
+
+    @Column(name = "last_name", length = 45, nullable = false)
+    private String last_name;
+
+    @Column(name = "email", length = 50, nullable = false)
+    private String email;
+    
+    @Column(name = "password", length = 20, nullable = false)
+    private String password;
+
+    @Column(name = "verification", length = 10, nullable = false)
+    private String verification;
+
+    @Column(name = "created_at", nullable = false)
+    private Date created_at;
+
+    public User() {
+    }
+
+    public User(int id, String first_name, String last_name, String email, String password, String verification, Date created_at) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+        this.verification = verification;
+        this.created_at = created_at;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVerification() {
+        return verification;
+    }
+
+    public void setVerification(String verification) {
+        this.verification = verification;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
