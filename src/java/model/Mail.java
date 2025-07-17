de78a0454f2ee1f59e8237061bc97a073031cbd3
xPamii <pbherath2018@gmail.com>
2025-07-18 00:50:09 +0530

    Mail sending class

---
+++ src/java/model/Mail.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author tharaka@javainstitute.edu.lk
+ */
+
+public class Mail {
+
+    private static final String APP_EMAIL = "pbherath2018@gmail.com";
+    private static final String APP_PASSWORD = "kopsulbrmfqxyeqk";
+
+    public static void sendMail(String email, String subject, String htmlContent) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(Mail.APP_EMAIL, Mail.APP_PASSWORD);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Mail.APP_EMAIL));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setContent(htmlContent,"text/html");
+
+            Transport.send(message);
+
+            System.out.println("Email sent successfully!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
