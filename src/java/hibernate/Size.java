5aced8eeee5369ad1bf0ad47a727aa317084704e
xPamii <pbherath2018@gmail.com>
2025-07-27 23:39:29 +0530

    size entity

---
+++ src/java/hibernate/Size.java
@@ -0,0 +1,42 @@
+package hibernate;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pamii
+ */
+@Entity
+@Table(name = "size")
+public class Size implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "value", length = 10, nullable = false)
+    private String value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
